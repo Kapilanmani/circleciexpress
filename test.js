7cec8edba6c1a714475ab04522f3969d7009772a
kapilanmani <kapilankapi468@gmai.com>
2023-12-29 11:15:05 +0530

    test.js

--- test.js
+++ test.js
@@ -10,7 +10,7 @@ describe("Example Test 1", () => {
     const mockResponse = httpMocks.createResponse();
     exampleRouteHandler(mockRequest, mockResponse);
     const actualResponseBody = mockResponse._getData();
-    const expectedResponseBody = "Goodbye kapilan";
+    const expectedResponseBody = "Goodbye kapi";
     assert(actualResponseBody, expectedResponseBody);
   });
 });
